dc03cc6f471383926cb45f54b6de33c234a55b90
kazuw0401 <kazumasaw0401@gmail.com>
2019-07-13 17:39:03 +0900

    formタグ内の名前、電話番号、メールアドレス、パスワード部分を作成
    スタイルを整えるため、今後cssの作成が必要

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+  <form action="#" method="post">
+    <p><label for="name">名前</label><input type="text" name="name" id="name"></p>
+    <p><label for="telNumber">電話番号</label><input type="number" name="telNumber" id="telNumber"></p>
+    <p><label for="email">メールアドレス</label><input type="mail" name="email" id="email"></p>
+    <p><label for="password">パスワード</label><input type="password" id="password"></p>
+    <input type="submit">
+  </form>
+</body>
+</html>
